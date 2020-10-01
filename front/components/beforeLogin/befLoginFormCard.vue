5b34d236064b38ffece17b7237475696d7947ba1
Ryo-cool <coolboxing576@gmail.com>
2020-10-02 06:10:14 +0900

    会員登録フォーム作成

---
+++ front/components/beforeLogin/befLoginFormCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container fluid>
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        cols="12"
+        class="my-8 text-center"
+      >
+        <h1 class="text-h5 font-weight-bold">
+          {{ appName }}に{{ pageTitle }}
+        </h1>
+      </v-col>
+
+      <v-card
+        flat
+        width="80%"
+        max-width="320"
+        color="transparent"
+      >
+        <!-- コンテンツを差し込むスロット -->
+        <slot name="form-card-content" />
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data ({ $route, $config: { appName } }) {
+    return {
+      appName,
+      pageTitle: this.$t(`pages.${$route.name}`)
+    }
+  }
+}
+</script>
