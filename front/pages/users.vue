267471dd0dbcb9361259f59bafbd424d2691f7f6
Ryo-cool <coolboxing576@gmail.com>
2020-10-03 06:44:11 +0900

    ユーザーページ作成

---
+++ front/pages/users.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    オブです
+  </div>
+</template>
+
+<script>
+export default {
+  layout ({ store }) {
+    return store.state.loggedIn ? 'loggedIn' : 'welcome'
+  }
+}
+</script>
