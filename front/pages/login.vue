0e65534787fa5fe80dda14a655fb40b130412fca
Ryo-cool <coolboxing576@gmail.com>
2020-10-02 18:57:57 +0900

    ログインメソッドを追加

--- front/pages/login.vue
+++ front/pages/login.vue
@@ -12,6 +12,26 @@
         :password.sync="params.auth.password"
         no-validation
       />
+      <v-card-actions>
+        <nuxt-link
+          to="#"
+          class="body-2 text-decoration-none"
+        >
+          パスワードを忘れた?
+        </nuxt-link>
+      </v-card-actions>
+      <v-card-text class="px-0">
+        <v-btn
+          :disabled="!isValid || loading"
+          :loading="loading"
+          block
+          color="myblue"
+          class="white--text"
+          @click="login"
+        >
+          ログインする
+        </v-btn>
+      </v-card-text>
     </v-form>
   </bef-login-form-card>
 </template>
@@ -24,6 +44,16 @@ export default {
       isValid: false,
       loading: false,
       params: { auth: { email: '', password: '' } }
+    }
+  },
+  methods: {
+    login () {
+      this.loading = true
+      setTimeout(() => {
+        this.$store.dispatch('login')
+        this.$router.replace('/')
+        this.loading = false
+      }, 1500)
     }
   }
 }
