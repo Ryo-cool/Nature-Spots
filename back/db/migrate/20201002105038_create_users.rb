26fb595b702bec7c9d9235bf70a4af094b4fad81
Ryo-cool <coolboxing576@gmail.com>
2020-10-02 19:55:50 +0900

    マイグレート

--- back/db/migrate/20201002105038_create_users.rb
+++ back/db/migrate/20201002105038_create_users.rb
@@ -1,7 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :password, null: false
+      t.string :image
+      t.text   :introduction
+      t.boolean :activated, default: false
+      t.boolean :admin, default: false
       t.timestamps
     end
   end
