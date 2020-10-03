5c9eadfa685020d3c7acddaff5f31fc35c648982
Ryo-cool <coolboxing576@gmail.com>
2020-10-03 09:17:49 +0900

    トークンからユーザーを取得するメソッド

--- back/app/services/user_auth/authenticator.rb
+++ back/app/services/user_auth/authenticator.rb
@@ -6,7 +6,7 @@ module UserAuth
       def token_from_request_headers
         request.headers["Authorization"]&.split&.last
       end
-      
+
       # クッキーのオブジェクトキー(config/initializers/user_auth.rb)
       def token_access_key
         UserAuth.token_access_key
@@ -15,6 +15,13 @@ module UserAuth
       # トークンの取得(リクエストヘッダー優先)
       def token
         token_from_request_headers || cookies[token_access_key]
+      end
+      
+      # トークンからユーザーを取得する
+      def fetch_entity_from_token
+        AuthToken.new(token: token).entity_for_user
+      rescue ActiveRecord::RecordNotFound, JWT::DecodeError, JWT::EncodeError
+        nil
       end
   end
 end
