d6de0eaf2f3f059521d7d43818e6a880f5f4c9d5
Ryo-cool <coolboxing576@gmail.com>
2020-10-03 09:11:45 +0900

    エンコード時のヘッダーオプションを指定

--- back/app/services/user_auth/auth_token.rb
+++ back/app/services/user_auth/auth_token.rb
@@ -27,10 +27,47 @@ module UserAuth
       def decode_key
         UserAuth.token_public_key || secret_key
       end
-      
+
       # アルゴリズム(config/initializers/user_auth.rb)
       def algorithm
         UserAuth.token_signature_algorithm
+      end
+      # オーディエンスの値がある場合にtrueを返す
+      def verify_audience?
+        UserAuth.token_audience.present?
+      end
+
+      # オーディエンス(config/initializers/user_auth.rb)
+      def token_audience
+        verify_audience? && UserAuth.token_audience.call
+      end
+
+      # トークン有効期限を秒数で返す
+      def token_lifetime
+        @lifetime.from_now.to_i
+      end
+      # デコード時オプション
+      # default: https://www.rubydoc.info/github/jwt/ruby-jwt/master/JWT/DefaultOptions
+      def decode_options
+        {
+          aud: token_audience,
+          verify_aud: verify_audience?,
+          algorithm: algorithm
+        }
+      end
+
+      # デフォルトクレーム
+      def claims
+        _claims = {}
+        _claims[:exp] = token_lifetime
+        _claims[:aud] = token_audience if verify_audience?
+        _claims
+      end
+      
+      # エンコード時のヘッダー
+      # Doc: https://openid-foundation-japan.github.io/draft-ietf-oauth-json-web-token-11.ja.html#typHdrDef
+      def header_fields
+        { typ: "JWT" }
       end
   end
 end
