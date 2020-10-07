440b40c245402b4569774961c2022f0847aa1f02
Ryo-cool <coolboxing576@gmail.com>
2020-10-07 17:16:34 +0900

    マイグレーションファイル記入

--- back/db/migrate/20201007081439_create_spots.rb
+++ back/db/migrate/20201007081439_create_spots.rb
@@ -1,7 +1,12 @@
 class CreateSpots < ActiveRecord::Migration[6.0]
   def change
     create_table :spots do |t|
-
+      t.string :name
+      t.text :introduction
+      t.string :location
+      t.string :photo
+      t.string :addess
+      t.integer :prefecture_id
       t.timestamps
     end
   end
