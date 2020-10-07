3c96d88bb415fc8e01d22209598b73741e85a93c
Ryo-cool <coolboxing576@gmail.com>
2020-10-07 17:17:37 +0900

    マイグレート

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_105038) do
+ActiveRecord::Schema.define(version: 2020_10_07_081439) do
+
+  create_table "spots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.text "introduction"
+    t.string "location"
+    t.string "photo"
+    t.string "addess"
+    t.integer "prefecture_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
