bf3a41109938aa0d77b508729c3915b7256ceb23
Ryo-cool <coolboxing576@gmail.com>
2020-10-14 23:01:55 +0900

    マイグレートでテーブル作成

--- back/db/migrate/20201014135945_create_reviews.rb
+++ back/db/migrate/20201014135945_create_reviews.rb
@@ -1,7 +1,12 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
-
+      t.string :title
+      t.text :text
+      t.string :wentday
+      t.integer :rating
+      t.string :image
+      t.references :spot, foreign_key: true
       t.timestamps
     end
   end
