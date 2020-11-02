9b10563349aadbd56a801989fd9985efc16aa1ff
Ryo-cool <coolboxing576@gmail.com>
2020-11-03 06:04:36 +0900

    マイページレイアウト

--- front/pages/account/settings.vue
+++ front/pages/account/settings.vue
@@ -1,7 +1,46 @@
 <template>
-  <div>
+  <v-container>
     {{ $route.fullPath }}
-  </div>
+    <v-row  justify="center" align-content="center">
+      <v-col cols="10">
+        <!-- ユーザー情報 -->
+        <v-card>
+          <v-container>
+            <v-row>
+              <v-col cols="2">
+                画像
+              </v-col>
+              <v-col cols="8">
+                <v-row>
+                  <v-col>
+                    投稿
+                  </v-col>
+                  <v-col>
+                    フォロー
+                  </v-col>
+                  <v-col> 
+                    フォロワー　
+                  </v-col>
+                  <v-col>
+                    プロフィール編集
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      
+        <v-row>
+          <v-col cols="7">
+            <mypage />
+          </v-col>
+          <v-col>
+            いきたいリスト
+          </v-col>
+        </v-row>   
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
