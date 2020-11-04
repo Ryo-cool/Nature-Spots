deb174ba61a3713531a2490393f4066b36eb2e53
Ryo-cool <coolboxing576@gmail.com>
2020-11-04 16:59:34 +0900

    ジャンル表示

--- back/app/controllers/api/v1/locations_controller.rb
+++ back/app/controllers/api/v1/locations_controller.rb
@@ -1,18 +1,25 @@
 class Api::V1::LocationsController < ApplicationController
   before_action :set_location, only: [:show]
-
+  before_action :set_spot, only: [:show]
   def index
     @locations= Location.all
     render json: @locations
   end
 
   def show
+    @reviews = @spot.review
     @jspot= Spot.where(location_id: @location.id)
-    render json:{location: @location,spot: @jspot} 
+    render json:{location: @location,spot: @jspot,review: @reviews} 
   end
 
   private
+
   def set_location
     @location = Location.find(params[:id])
   end
+
+  def set_spot
+    @spot = Spot.find(params[:id])
+  end
+
 end
--- front/pages/location/_id.vue
+++ front/pages/location/_id.vue
@@ -1,15 +1,24 @@
 <template>
   <v-container>
+    {{spot.name}}のスポット一覧
     <v-row>
-      {{spot.name}}のスポット一覧
       <v-col 
         v-for="jspot in jspots"
         :key="jspot"
+        cols="12"
+        sm="4"
         >
         <v-card>
-          <v-card-title>
-            {{ jspot.name}}
-          </v-card-title>
+          <nuxt-link
+          :to="$my.spotLinkTo(jspot.id)"
+          class="text-decoration-none"
+          >
+            <v-img
+            src="https://picsum.photos/id/1036/960/540"
+            ></v-img>
+            <v-card-title>{{ jspot.name }}</v-card-title>
+            <v-card-text v-text="jspot.introduction"/>
+          </nuxt-link>
         </v-card>
       </v-col>
     </v-row>
@@ -22,6 +31,7 @@ export default {
   data () {
     return {
       spot: {},
+      review: {},
       jspots: []
     }
   },
@@ -34,6 +44,7 @@ export default {
       .then((res) => {
         this.spot = res.data.location.attributes
         this.jspots = res.data.spot
+        this.review = res.data.review
         
       })
       .catch((error) => {
