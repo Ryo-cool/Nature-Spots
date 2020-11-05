00be7515108157a9ceeac5fcf6f6d95568ea0b76
Ryo-cool <coolboxing576@gmail.com>
2020-11-06 02:51:19 +0900

    各メソッドの作成
    ・有効期限を作成・ストレージを削除・有効期限を取得・有効期限内であるかを判定・ログイン業務・ログアウト業務

--- front/plugins/auth.js
+++ front/plugins/auth.js
@@ -1,7 +1,39 @@
+const storage = window.localStorage
+const keys = { exp: 'exp' }
+
 class Authentication {
   constructor (ctx) {
     this.store = ctx.store
     this.$axios = ctx.$axios
+  }
+  // storageに保存
+  setStorage (exp) {
+    storage.setItem(keys.exp, exp * 1000)
+  }
+  // storageを削除
+  removeStorage () {
+    for (const key of Object.values(keys)) {
+      storage.removeItem(key)
+    }
+  }
+  // storageの有効期限を複合して返す
+  getExpire () {
+    return storage.getItem(keys.exp)
+  }
+  // 有効期限内の場合はtrueを返す
+  isAuthenticated () {
+    return new Date().getTime() < this.getExpire()
+  }
+  // ログイン業務
+  login ({ exp, user }) {
+    this.setStorage(exp)
+    this.store.dispatch('getCurrentUser', user)
+  }
+  // ログアウト業務
+  logout () {
+    this.$axios.$delete('/api/v1/user_token')
+    this.removeStorage()
+    this.store.dispatch('getCurrentUser', null)
   }
 }
 
