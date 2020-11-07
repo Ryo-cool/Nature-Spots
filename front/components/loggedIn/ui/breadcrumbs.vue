7dee8a6fd74558d6540007bf8f8a248543e42283
Ryo-cool <coolboxing576@gmail.com>
2020-11-07 11:22:41 +0900

    パンくず表示

---
+++ front/components/loggedIn/ui/breadcrumbs.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-breadcrumbs
+    :items="items"
+  >
+    <template v-slot:item="{ item }">
+      <v-breadcrumbs-item
+        exact
+        class="text-truncate"
+      >
+        <nuxt-link to="/">HOME</nuxt-link> 
+        <div style="white-space: pre-wrap">  > </div>
+        {{ item.text }}
+      </v-breadcrumbs-item>
+    </template>
+    <template v-slot:divider>
+      <v-icon>
+        mdi-chevron-right
+      </v-icon>
+    </template>
+  </v-breadcrumbs>
+</template>
+
+<script>
+export default {
+  computed: {
+    items () {
+      return [
+        { text: this.$my.pageTitle(this.$route.name) }
+      ]
+    }
+  }
+}
+</script>
