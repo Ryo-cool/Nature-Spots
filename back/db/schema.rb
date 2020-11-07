e3b0b33def15c5b622f715f406e1a0f6804f69d8
Ryo-cool <coolboxing576@gmail.com>
2020-11-07 12:51:10 +0900

    モデル作成＆マイグレート

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_135945) do
+ActiveRecord::Schema.define(version: 2020_11_07_034850) do
+
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "review_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["review_id"], name: "index_likes_on_review_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
@@ -49,5 +58,7 @@ ActiveRecord::Schema.define(version: 2020_10_14_135945) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "likes", "reviews"
+  add_foreign_key "likes", "users"
   add_foreign_key "reviews", "spots"
 end
