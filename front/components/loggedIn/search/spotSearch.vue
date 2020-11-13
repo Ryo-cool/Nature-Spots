86d4b1c37b2b6e003c5b7e8938a0466307935ec2
Ryo-cool <coolboxing576@gmail.com>
2020-11-13 18:42:13 +0900

    オートコンプリート
    spotテーブルから検索候補となるデータをNuxtに送り、検索窓に表示させる

--- front/components/loggedIn/search/spotSearch.vue
+++ front/components/loggedIn/search/spotSearch.vue
@@ -1,17 +1,39 @@
 <template>
-  <v-form>
-    
-    <v-text-field
-      label="Search"
+  <div>
+    <v-autocomplete
+      v-model="value"
+      :items="spots"
+      item-text="name"
+      prepend-inner-icon="mdi-database-search"
+      dense
       solo
-      prepend-inner-icon="mdi-magnify"
       height="60"
-    >
-    </v-text-field>
-  </v-form>
+      hide-no-data
+      label="行き先は？"
+    ></v-autocomplete>
+  </div>
 </template>
 
 <script>
+
+import axios from "~/plugins/axios"
+
 export default {
+  data () {
+    return {
+      spots: [],
+      value: null,
+      
+    }
+  },
+  created() {
+    // ユーザーをaxiosで取得
+    this.$axios.get("/api/v1/spots").then(res => {
+      if (res.data) {
+        this.spots = res.data.spots
+
+      }
+    })
+  }
 }
 </script>
