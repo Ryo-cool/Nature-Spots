b3195e8bf98aeeefeb9b1c591799abc1864e2a94
Ryo-cool <coolboxing576@gmail.com>
2020-11-22 17:03:58 +0900

    マイグレート
    お気に入りテーブルを作成

---
+++ back/db/migrate/20201122080043_create_favorites.rb
@@ -0,0 +1,9 @@
+class CreateFavorites < ActiveRecord::Migration[6.0]
+  def change
+    create_table :favorites do |t|
+      t.references :spot, null: false, foreign_key:true
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
