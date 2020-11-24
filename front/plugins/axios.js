090058577b9a64ef7ecdee84a62546a58a05df18
Ryo-cool <coolboxing576@gmail.com>
2020-11-25 00:17:03 +0900

    baseURL

--- front/plugins/axios.js
+++ front/plugins/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export default ({ $axios, isDev }) => {
+  baseURL: "https://www.nature-spots.work"
   // リクエストログ
   $axios.onRequest((config) => {
     // if (isDev) 追加
