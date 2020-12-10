8ad85dda46758fb8e2bdda738b0180fb5488624d
Ryo-cool <coolboxing576@gmail.com>
2020-12-11 06:38:24 +0900

    config

--- back/config/initializers/carriewave.rb
+++ back/config/initializers/carriewave.rb
@@ -1,5 +1,6 @@
 
-require_relative "constants"
+# require_relative "constants"
 CarrierWave.configure do |config|
-  config.asset_host = Constants::URL
+  # config.asset_host = Constants::URL
+  config.asset_host = "http://localhost:3000/"
 end
--- back/config/initializers/constants.rb
+++
@@ -1,3 +0,0 @@
-module Constants
-    URL = "http://localhost:3000/"
-end
